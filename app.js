a5af31d56a56b4d9974daedda4834dd86491bd72
Elisei <strashevsky.elisey@gmail.com>
2022-02-08 12:10:06 +0300

    simple distance count with date-fns function

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-New;
+import { format, getUnixTime, formatDistanceToNow } from "date-fns";
+
+const UI = {
+  startTime: document.querySelector(".input"),
+  btnOk: document.querySelector(".OK"),
+  endTime: document.querySelector(".timeresult"),
+};
+
+UI.btnOk.addEventListener("click", countDownTime);
+
+function countDownTime() {
+  const distanceTime = formatDistanceToNow(new Date(UI.startTime.value));
+  console.log(distanceTime);
+  UI.endTime.textContent = `... ${distanceTime}!`;
+}
